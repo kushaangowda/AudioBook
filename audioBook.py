75a9e4ba17ebf70de89c5246cbaf5cf992fda91b
Kushaan Gowda <kushaangowda@yahoo.com>
2021-02-03 13:25:44 +0530

    added new features

--- audioBook.py
+++ audioBook.py
@@ -9,7 +9,9 @@ voices = engine.getProperty('voices')
 male_voice = voices[0].id
 female_voice = voices[1].id
 
-def clear(): 
+audioBooks = ['sample.pdf', 'sample2.pdf']
+
+def clearCMD(): 
     # for windows 
     if name == 'nt': 
         _ = system('cls')
@@ -51,13 +53,42 @@ def playAudioBook(name):
     engine.say('Audiobook has ended')
     engine.runAndWait() 
 
+def chooseAudioBooks():
+    c = 1
+    while c == 1:
+        clearCMD()
+        i = 0
+
+        print('Choose a book: ')
+        for book in audioBooks:
+            print(str(i)+': '+book)
+            i = i+1
+
+        print('Enter the S.no of book, or q to go back to home screen')
+        a = input()
+
+        if a.isnumeric():
+            a = int(a)
+            if a<i:
+                playAudioBook(audioBooks[a])
+            else:
+                print('Invalid S.no')
+        elif a == 'q' or a == 'Q':
+            c = 0
+            continue
+        else:
+            print('Invalid S.no')
+
+        sleep(1)
+
 def runMain():
     c = 1
+
     while c == 1:
         print('Choose one among the following: ')
         print('1: change voice to male')
         print('2: change voice to female')
-        print('3: play audiobook')
+        print('3: Choose audiobook')
         print('q: to quit')
         a = input()
 
@@ -66,13 +97,14 @@ def runMain():
         elif a == '2':
             changeVoice('female')
         elif a == '3':
-            playAudioBook('sample.pdf')
+            chooseAudioBooks()
         elif a == 'q' or a == 'Q':
             c = 0
             print('Ending Application')
         else:
             print('Invalid Input')
+
         sleep(1)
-        clear()
+        clearCMD()
 
 runMain()
